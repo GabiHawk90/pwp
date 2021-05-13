5c87e2fffd2ce69c32d98c08e6a76f1876dcd74e
Gabriella Hawkins <ghawkins3@cnm.edu>
2021-05-12 21:59:36 -0600

    working on hr style

--- public_html/documentatioon/Milestone-3/Script.js
+++ public_html/documentatioon/Milestone-3/Script.js
@@ -1,10 +1,9 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
 })
 
 
 
+// Unneccesary stagger animation of the section elements
+TweenMax.staggerFrom("section", .65, {top: "+=60px", opacity: 0, ease:Strong.easeOut}, 0.1);
 
